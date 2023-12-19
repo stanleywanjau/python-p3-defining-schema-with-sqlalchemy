22dad9748165a388762e342f3cd8946d056c56cc
stanleywanjau <stanleywanjau371@gmail.com>
2023-12-19 21:45:42 +0300

    completed

--- lib/debug.py
+++ lib/debug.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
 from sqlalchemy import create_engine
+from sqlalchemy_sandbox import Base, Student
 
-from sqlalchemy_sandbox import Student
+# engine = create_engine('sqlite:///students.db')
+# Base.metadata.create_all(engine)
 
-engine = create_engine('sqlite:///students.db')
-
+# Creating a session to interact with the database
+# from sqlalchemy.orm import sessionmaker
+# Session = sessionmaker(bind=engine)
+# session = Session()
 if __name__ == '__main__':
     import ipdb; ipdb.set_trace()
